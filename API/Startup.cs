6eefcd073679dc7c5e499fd6534add5117da1d28
Karthik SK <karthik.sk@ef.com>
2020-12-28 14:54:47 +0530

    Added client module

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,14 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -48,6 +56,8 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
